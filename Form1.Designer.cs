80da91b89cf9c10b109bf0e102cc25322061e898
christianM <cristianmg3099@gmail.com>
2021-05-08 13:36:15 -0600

    Agregados los 3 comandos necesarios , falta dividir la cadena y darle mejor aspecto ala interfaz

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,438 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.PanelMenu = new System.Windows.Forms.Panel();
+			this.btn_Conectar = new System.Windows.Forms.Button();
+			this.btn_Configuraciones = new System.Windows.Forms.Button();
+			this.btn_Panel = new System.Windows.Forms.Button();
+			this.PanelLogo = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.panelfecha = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.txt_TempMax = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.lbl_Fecha = new System.Windows.Forms.Label();
+			this.lbl_hora = new System.Windows.Forms.Label();
+			this.txt_Cadena = new System.Windows.Forms.TextBox();
+			this.lbl_tiempo = new System.Windows.Forms.Label();
+			this.txt_tiempo = new System.Windows.Forms.TextBox();
+			this.btn_Tiempo = new System.Windows.Forms.Button();
+			this.btn_request = new System.Windows.Forms.Button();
+			this.PanelMenu.SuspendLayout();
+			this.PanelLogo.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panelfecha.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// PanelMenu
+			// 
+			this.PanelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(70)))));
+			this.PanelMenu.Controls.Add(this.btn_request);
+			this.PanelMenu.Controls.Add(this.btn_Tiempo);
+			this.PanelMenu.Controls.Add(this.txt_tiempo);
+			this.PanelMenu.Controls.Add(this.lbl_tiempo);
+			this.PanelMenu.Controls.Add(this.btn_Configuraciones);
+			this.PanelMenu.Controls.Add(this.btn_Panel);
+			this.PanelMenu.Controls.Add(this.PanelLogo);
+			this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+			this.PanelMenu.Location = new System.Drawing.Point(0, 0);
+			this.PanelMenu.Name = "PanelMenu";
+			this.PanelMenu.Size = new System.Drawing.Size(197, 698);
+			this.PanelMenu.TabIndex = 3;
+			// 
+			// btn_Conectar
+			// 
+			this.btn_Conectar.FlatAppearance.BorderSize = 0;
+			this.btn_Conectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_Conectar.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+			this.btn_Conectar.Location = new System.Drawing.Point(0, 3);
+			this.btn_Conectar.Name = "btn_Conectar";
+			this.btn_Conectar.Size = new System.Drawing.Size(197, 91);
+			this.btn_Conectar.TabIndex = 6;
+			this.btn_Conectar.Text = "Conectar";
+			this.btn_Conectar.UseVisualStyleBackColor = true;
+			this.btn_Conectar.Click += new System.EventHandler(this.btn_Conectar_Click);
+			// 
+			// btn_Configuraciones
+			// 
+			this.btn_Configuraciones.Dock = System.Windows.Forms.DockStyle.Top;
+			this.btn_Configuraciones.FlatAppearance.BorderSize = 0;
+			this.btn_Configuraciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_Configuraciones.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.btn_Configuraciones.Location = new System.Drawing.Point(0, 147);
+			this.btn_Configuraciones.Name = "btn_Configuraciones";
+			this.btn_Configuraciones.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+			this.btn_Configuraciones.Size = new System.Drawing.Size(197, 40);
+			this.btn_Configuraciones.TabIndex = 3;
+			this.btn_Configuraciones.Text = "Identificar";
+			this.btn_Configuraciones.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btn_Configuraciones.UseVisualStyleBackColor = true;
+			this.btn_Configuraciones.Click += new System.EventHandler(this.btn_Configuraciones_Click);
+			// 
+			// btn_Panel
+			// 
+			this.btn_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.btn_Panel.FlatAppearance.BorderSize = 0;
+			this.btn_Panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_Panel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.btn_Panel.Location = new System.Drawing.Point(0, 97);
+			this.btn_Panel.Name = "btn_Panel";
+			this.btn_Panel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+			this.btn_Panel.Size = new System.Drawing.Size(197, 50);
+			this.btn_Panel.TabIndex = 3;
+			this.btn_Panel.Text = "Panel";
+			this.btn_Panel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btn_Panel.UseVisualStyleBackColor = true;
+			// 
+			// PanelLogo
+			// 
+			this.PanelLogo.Controls.Add(this.btn_Conectar);
+			this.PanelLogo.Controls.Add(this.pictureBox2);
+			this.PanelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+			this.PanelLogo.Location = new System.Drawing.Point(0, 0);
+			this.PanelLogo.Name = "PanelLogo";
+			this.PanelLogo.Size = new System.Drawing.Size(197, 97);
+			this.PanelLogo.TabIndex = 2;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(197, 94);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 3;
+			this.pictureBox2.TabStop = false;
+			// 
+			// panelfecha
+			// 
+			this.panelfecha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(70)))));
+			this.panelfecha.Controls.Add(this.panel1);
+			this.panelfecha.Controls.Add(this.lbl_Fecha);
+			this.panelfecha.Controls.Add(this.lbl_hora);
+			this.panelfecha.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelfecha.Location = new System.Drawing.Point(197, 0);
+			this.panelfecha.Name = "panelfecha";
+			this.panelfecha.Size = new System.Drawing.Size(870, 100);
+			this.panelfecha.TabIndex = 7;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label18);
+			this.panel1.Controls.Add(this.label17);
+			this.panel1.Controls.Add(this.label16);
+			this.panel1.Controls.Add(this.label15);
+			this.panel1.Controls.Add(this.label14);
+			this.panel1.Controls.Add(this.label13);
+			this.panel1.Controls.Add(this.label12);
+			this.panel1.Controls.Add(this.label11);
+			this.panel1.Controls.Add(this.textBox4);
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.txt_TempMax);
+			this.panel1.Controls.Add(this.label10);
+			this.panel1.Controls.Add(this.label9);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(3, 97);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(792, 591);
+			this.panel1.TabIndex = 7;
+			this.panel1.Visible = false;
+			// 
+			// label18
+			// 
+			this.label18.AutoSize = true;
+			this.label18.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label18.Location = new System.Drawing.Point(204, 415);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(132, 17);
+			this.label18.TabIndex = 15;
+			this.label18.Text = "Distancia necesaria";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label17.Location = new System.Drawing.Point(204, 322);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(140, 17);
+			this.label17.TabIndex = 14;
+			this.label17.Text = "Luminosidad Minima ";
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label16.Location = new System.Drawing.Point(224, 239);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(117, 17);
+			this.label16.TabIndex = 13;
+			this.label16.Text = "Humedad Minima";
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label15.Location = new System.Drawing.Point(146, 168);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(195, 17);
+			this.label15.TabIndex = 12;
+			this.label15.Text = "Valor Minimo de Temperatura";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label14.Location = new System.Drawing.Point(146, 128);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(198, 17);
+			this.label14.TabIndex = 11;
+			this.label14.Text = "Valor Maximo de Temperatura";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label13.Location = new System.Drawing.Point(357, 390);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(93, 17);
+			this.label13.TabIndex = 10;
+			this.label13.Text = "Refrigeracion";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label12.Location = new System.Drawing.Point(357, 302);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(93, 17);
+			this.label12.TabIndex = 9;
+			this.label12.Text = "Refrigeracion";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label11.Location = new System.Drawing.Point(357, 216);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(93, 17);
+			this.label11.TabIndex = 8;
+			this.label11.Text = "Refrigeracion";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(350, 410);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 22);
+			this.textBox4.TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(350, 236);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 22);
+			this.textBox3.TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(350, 322);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(350, 168);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 4;
+			// 
+			// txt_TempMax
+			// 
+			this.txt_TempMax.Location = new System.Drawing.Point(350, 128);
+			this.txt_TempMax.Name = "txt_TempMax";
+			this.txt_TempMax.Size = new System.Drawing.Size(100, 22);
+			this.txt_TempMax.TabIndex = 3;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label10.Location = new System.Drawing.Point(357, 99);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(93, 17);
+			this.label10.TabIndex = 2;
+			this.label10.Text = "Refrigeracion";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Century", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label9.Location = new System.Drawing.Point(115, 18);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(597, 47);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "Modificacion de los parametros";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(559, 201);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(86, 70);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
+			// 
+			// lbl_Fecha
+			// 
+			this.lbl_Fecha.AutoSize = true;
+			this.lbl_Fecha.Font = new System.Drawing.Font("Century", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_Fecha.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.lbl_Fecha.Location = new System.Drawing.Point(184, 9);
+			this.lbl_Fecha.Name = "lbl_Fecha";
+			this.lbl_Fecha.Size = new System.Drawing.Size(31, 33);
+			this.lbl_Fecha.TabIndex = 1;
+			this.lbl_Fecha.Text = "0";
+			// 
+			// lbl_hora
+			// 
+			this.lbl_hora.AutoSize = true;
+			this.lbl_hora.Font = new System.Drawing.Font("Century", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_hora.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.lbl_hora.Location = new System.Drawing.Point(238, 50);
+			this.lbl_hora.Name = "lbl_hora";
+			this.lbl_hora.Size = new System.Drawing.Size(46, 50);
+			this.lbl_hora.TabIndex = 0;
+			this.lbl_hora.Text = "0";
+			// 
+			// txt_Cadena
+			// 
+			this.txt_Cadena.Location = new System.Drawing.Point(359, 111);
+			this.txt_Cadena.Name = "txt_Cadena";
+			this.txt_Cadena.Size = new System.Drawing.Size(486, 22);
+			this.txt_Cadena.TabIndex = 8;
+			// 
+			// lbl_tiempo
+			// 
+			this.lbl_tiempo.AutoSize = true;
+			this.lbl_tiempo.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.lbl_tiempo.Location = new System.Drawing.Point(46, 287);
+			this.lbl_tiempo.Name = "lbl_tiempo";
+			this.lbl_tiempo.Size = new System.Drawing.Size(97, 17);
+			this.lbl_tiempo.TabIndex = 9;
+			this.lbl_tiempo.Text = "Tiempo = 0ms";
+			// 
+			// txt_tiempo
+			// 
+			this.txt_tiempo.Location = new System.Drawing.Point(12, 322);
+			this.txt_tiempo.Name = "txt_tiempo";
+			this.txt_tiempo.Size = new System.Drawing.Size(152, 22);
+			this.txt_tiempo.TabIndex = 9;
+			this.txt_tiempo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_tiempo_KeyPress);
+			// 
+			// btn_Tiempo
+			// 
+			this.btn_Tiempo.FlatAppearance.BorderSize = 0;
+			this.btn_Tiempo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_Tiempo.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+			this.btn_Tiempo.Location = new System.Drawing.Point(33, 355);
+			this.btn_Tiempo.Name = "btn_Tiempo";
+			this.btn_Tiempo.Size = new System.Drawing.Size(110, 37);
+			this.btn_Tiempo.TabIndex = 9;
+			this.btn_Tiempo.Text = "Cambiar";
+			this.btn_Tiempo.UseVisualStyleBackColor = true;
+			this.btn_Tiempo.Click += new System.EventHandler(this.btn_Tiempo_Click);
+			// 
+			// btn_request
+			// 
+			this.btn_request.Dock = System.Windows.Forms.DockStyle.Top;
+			this.btn_request.FlatAppearance.BorderSize = 0;
+			this.btn_request.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_request.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.btn_request.Location = new System.Drawing.Point(0, 187);
+			this.btn_request.Name = "btn_request";
+			this.btn_request.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+			this.btn_request.Size = new System.Drawing.Size(197, 40);
+			this.btn_request.TabIndex = 10;
+			this.btn_request.Text = "Solicitar Variables";
+			this.btn_request.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btn_request.UseVisualStyleBackColor = true;
+			this.btn_request.Click += new System.EventHandler(this.btn_request_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(59)))), ((int)(((byte)(104)))));
+			this.ClientSize = new System.Drawing.Size(1067, 698);
+			this.Controls.Add(this.txt_Cadena);
+			this.Controls.Add(this.panelfecha);
+			this.Controls.Add(this.PanelMenu);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.PanelMenu.ResumeLayout(false);
+			this.PanelMenu.PerformLayout();
+			this.PanelLogo.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panelfecha.ResumeLayout(false);
+			this.panelfecha.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel PanelMenu;
+		private System.Windows.Forms.Button btn_Conectar;
+		private System.Windows.Forms.Button btn_Configuraciones;
+		private System.Windows.Forms.Button btn_Panel;
+		private System.Windows.Forms.Panel PanelLogo;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Panel panelfecha;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label18;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox txt_TempMax;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label lbl_Fecha;
+		private System.Windows.Forms.Label lbl_hora;
+		private System.Windows.Forms.TextBox txt_Cadena;
+		private System.Windows.Forms.Button btn_Tiempo;
+		private System.Windows.Forms.TextBox txt_tiempo;
+		private System.Windows.Forms.Label lbl_tiempo;
+		private System.Windows.Forms.Button btn_request;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,131 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Practica_4
 {
 	public partial class Form1 : Form
 	{
+		SerialPort Lora;
+		String mensaje;
+		Timer timerControl;
+
+
+		string[] ValoresSensores;
+		string Valores;
+
 		public Form1()
 		{
 			InitializeComponent();
+			Lora= new SerialPort();
+			Lora.BaudRate = 9600;
+			Lora.PortName = "COM3";
+			Lora.DataReceived += Lora_DataReceived;
+
+
+			//Timer
+			timerControl = new Timer();
+			timerControl.Interval = 250;
+			timerControl.Enabled = true;
+			timerControl.Tick += TimerControl_Tick;	
+		}
+
+		private void TimerControl_Tick(object sender, EventArgs e)
+		{
+			txt_Cadena.Text = mensaje;
+		}
+
+		private void Lora_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			SolicitarValores(2);
+		}
+
+		private void btn_Configuraciones_Click(object sender, EventArgs e)
+		{
+			if (Lora.IsOpen)
+			{
+				Lora.WriteLine("~$1/25"); //El $=36  codigo ASCII
+			}
+		}
+
+		private void btn_Conectar_Click(object sender, EventArgs e)
+		{
+			if (!Lora.IsOpen && btn_Conectar.Text == "Conectar")
+			{
+				try
+				{
+					Lora.Open();
+					btn_Conectar.Text = "Desconectar";
+					timerControl.Start();
+
+
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message + "\nVerificar el COM de su dispositivo", "Fallo del COM");
+
+					timerControl.Stop();
+
+				}
+
+			}
+			else if (Lora.IsOpen && btn_Conectar.Text == "Desconectar")
+			{
+				Lora.Close();
+				btn_Conectar.Text = "Conectar";
+				timerControl.Stop();
+			}
+		}
+
+		private void txt_tiempo_KeyPress(object sender, KeyPressEventArgs e)
+		{ //Solo permitir numeros en el textbox del tiempo
+			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void btn_Tiempo_Click(object sender, EventArgs e)
+		{
+			if (Lora.IsOpen)
+			{
+				Lora.WriteLine("~*1/26/"+txt_tiempo.Text); //El *=42  codigo ASCII
+				lbl_tiempo.Text = "Tiempo = " + txt_tiempo.Text +"ms";
+				txt_tiempo.Text = "";	
+			}
+
+		}
+
+		private void btn_request_Click(object sender, EventArgs e)
+		{
+			if (Lora.IsOpen)
+			{
+				Lora.WriteLine("~$1/20"); //El $=42  codigo ASCII
+				
+			}
+		}
+
+		void SolicitarValores(int Length)
+		{
+			try
+			{
+				if (Lora.BytesToRead > Length)
+				{
+					mensaje = Lora.ReadLine();
+					ValoresSensores = mensaje.Split('/');
+				}
+
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+
+				timerControl.Stop();
+
+			}
+
 		}
 	}
 }
